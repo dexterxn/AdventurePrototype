8233cf46077cf5dff7be473988b9afc148481228
Dexter Zhang <dejzhang@ucsc.edu>
2023-05-10 12:01:28 -0700

    Dexter Zhang, updated name of the game

--- dextergame.js
+++ dextergame.js
@@ -415,7 +415,7 @@ class Intro extends Phaser.Scene {
         super('intro')
     }
     create() {
-        this.add.text(50,50, "Adventure awaits!").setFontSize(50);
+        this.add.text(50,50, "Joe Bob's Adventure Part 3").setFontSize(50);
         this.add.text(50,100, "Click anywhere to begin.").setFontSize(20);
         this.input.on('pointerdown', () => {
             this.cameras.main.fade(1000, 0,0,0);
